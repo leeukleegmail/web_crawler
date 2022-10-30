3403c55537ee2f430985ebb14ed98d879e103473
Your Name <pi@example.com>
2022-10-30 16:12:55 +0100

    update docker

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ FROM python:3.8-slim-buster
 WORKDIR /python-docker
 
 COPY requirements.txt requirements.txt
+RUN /usr/local/bin/python -m pip install --upgrade pip
 RUN pip3 install -r requirements.txt
 
 CMD [ "python3", "server.py"]
--- docker.sh
+++ docker.sh
@@ -1,3 +1,5 @@
+docker stop python-docker
+docker rm python-docker
 docker build --tag python-docker .
-docker run -d -p 5000:5000 -v $(pwd)/:/python-docker python-docker
+docker run -d -p 5000:5000 --name python-docker --restart unless-stopped -v $(pwd)/:/python-docker python-docker
 
